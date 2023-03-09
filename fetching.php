416c18db26868a03eb19f780981ec8a0cd917c35
johnxiler <johnxiler@gmail.com>
2023-03-10 03:46:06 +0800

    some changes

--- fetching.php
+++ fetching.php
@@ -47,6 +47,8 @@ if ($result->num_rows > 0) {
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fetch API Example</title>
     <link rel="stylesheet" href="style.css">
   </head>
