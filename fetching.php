d1b58dcc46df5dd2ccf823caa30ca7b6b70baba3
johnxiler <johnxiler@gmail.com>
2023-03-12 17:29:40 +0800

    .

--- fetching.php
+++ fetching.php
@@ -6,7 +6,7 @@ $password = '';
 $dbname = 'book_db';
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 // Retrieve the list of books from the database
@@ -20,22 +20,22 @@ if ($conn->connect_error) {
 // }
 // Retrieve the list of books from the database using a prepared statement
 if (isset($_GET['search'])) {
-$stmt = $conn->prepare("SELECT `id`, `title`, 
+  $stmt = $conn->prepare("SELECT `id`, `title`, 
                                 `author`, `year`, `average_rating`, 
                                 `isbn`, `isbn13`, `language_code`, 
                                 `num_pages`, `ratings_count`, 
                                 `text_reviews_count`, `publication_date` 
                                 FROM books WHERE title LIKE ? OR author LIKE ?");
-$search = '%' . htmlspecialchars($_GET['search']) . '%'; // Sanitize user input to prevent XSS attacks
-$stmt->bind_param("ss", $search, $search);
-$stmt->execute();
-$result = $stmt->get_result();
-$books = array();
-if ($result->num_rows > 0) {
+  $search = '%' . htmlspecialchars($_GET['search']) . '%'; // Sanitize user input to prevent XSS attacks
+  $stmt->bind_param("ss", $search, $search);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $books = array();
+  if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $books[] = $row;
+      $books[] = $row;
     }
-}
+  }
 }
 // Set the content type to JSON
 //header('Content-Type: application/json'); //to treat as json file rendered in the website.
@@ -46,65 +46,68 @@ if ($result->num_rows > 0) {
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fetch API Example</title>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <h1>Books</h1>
-    <form method="get">
-      <input type="text" name="search" placeholder="Search for a book...">
-      <button type="submit">Search</button>
-    </form>
-    <table id="book-list">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Year</th>
-          <th>average_rating</th>
-          <th>isbn</th>
-          <th>isbn13</th>
-          <th>language_code</th> 
-          <th>num_pages</th>
-          <th>ratings_count</th> 
-          <th>text_reviews_count</th>
-          <th>publication_date</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php foreach ($books as $book): ?>
-          <tr>
-            <td><?= htmlspecialchars($book['title']) ?></td> <!-- Sanitize output to prevent XSS attacks -->
-            <td><?= htmlspecialchars($book['author']) ?></td>
-            <td><?= $book['year'] ?></td>
-            <td><?= htmlspecialchars($book['average_rating']) ?></td>
-            <td><?= htmlspecialchars($book['isbn']) ?></td>
-            <td><?= htmlspecialchars($book['isbn13']) ?></td>
-            <td><?= htmlspecialchars($book['language_code']) ?></td> 
-            <td><?= $book['num_pages'] ?></td>
-            <td><?= $book['ratings_count'] ?></td> 
-            <td><?= htmlspecialchars($book['text_reviews_count']) ?></td>
-            <td><?= $book['publication_date'] ?></td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
 
-    <script>
-      fetch('<?= $_SERVER['PHP_SELF'] ?>')
-        .then(response => response.json())
-        .then(data => {
-          const bookList = document.getElementById('book-list');
-          data.forEach(book => {
-            const li = document.createElement('li');
-            li.textContent = `${book.title} by ${book.author} (${book.year})`;
-            bookList.appendChild(li);
-          });
-        })
-        .catch(error => console.error(error));
-    </script>
-  </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fetch API Example</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>Books</h1>
+  <form method="get">
+    <input type="text" name="search" placeholder="Search for a book...">
+    <button type="submit">Search</button>
+  </form>
+  <table id="book-list">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Year</th>
+        <th>average_rating</th>
+        <th>isbn</th>
+        <th>isbn13</th>
+        <th>language_code</th>
+        <th>num_pages</th>
+        <th>ratings_count</th>
+        <th>text_reviews_count</th>
+        <th>publication_date</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($books as $book) : ?>
+        <tr>
+          <td><?= htmlspecialchars($book['title']) ?></td> <!-- Sanitize output to prevent XSS attacks -->
+          <td><?= htmlspecialchars($book['author']) ?></td>
+          <td><?= $book['year'] ?></td>
+          <td><?= htmlspecialchars($book['average_rating']) ?></td>
+          <td><?= htmlspecialchars($book['isbn']) ?></td>
+          <td><?= htmlspecialchars($book['isbn13']) ?></td>
+          <td><?= htmlspecialchars($book['language_code']) ?></td>
+          <td><?= $book['num_pages'] ?></td>
+          <td><?= $book['ratings_count'] ?></td>
+          <td><?= htmlspecialchars($book['text_reviews_count']) ?></td>
+          <td><?= $book['publication_date'] ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+  <script>
+    fetch('<?= $_SERVER['PHP_SELF'] ?>')
+      .then(response => response.json())
+      .then(data => {
+        const bookList = document.getElementById('book-list');
+        data.forEach(book => {
+          const li = document.createElement('li');
+          li.textContent = `${book.title} by ${book.author} (${book.year})`;
+          bookList.appendChild(li);
+        });
+      })
+      .catch(error => console.error(error));
+  </script>
+</body>
+
 </html>
